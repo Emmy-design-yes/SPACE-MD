090649e5044e260109d65740bff6b06af5f3b8ee
Emmy-design-yes <emmanuelighata73@gmail.com>
2025-08-08 12:40:51 +0100

    settings.js

--- settings.js
+++ settings.js
@@ -2,16 +2,16 @@ require('dotenv').config();
 
 module.exports = {
   //Enter your session id here (optional)
-  SESSION_ID: process.env.SESSION_ID || '',
+  SESSION_ID: process.env.SESSION_ID || 'CYPHER-X:~UEsDBBQAAAgIAAVbCFs71XExTwQAAG8HAAAKAAAAY3JlZHMuanNvbpVU2a6jOBD9F78m6hDWgHSlIWQhC1wI2Uf94IABJ2yxTQi08u8jkr59+2GmdYcnUy6dOnXqlH+ALMcULVANtB+gIPgGGWqPrC4Q0MCwDENEQBcEkEGgAd1uOmU9irZZjGN5SupFAK9+sycVXMedwEbp0PZdQ0l3+ht4dEFRnhLs*wHwPNvGk4014*K8PLr9reGOLmLCDSfifNKxqkRPjhHX99QAW2*g0SJCTHAWjYsYpYjAZIFqB2LyNfrypN71Ok6Q88KNY9UkiML1GPL5Ibfu72d9z46N4sHULqfi1+iPzRnF8dTx08H7+nKz7F1QnzN5kHHhedhbZ0rhjPq0N1km7os+xVGGglmAMoZZ*WXdXdNfGvp7Q47NUXTsAcdtR8p417kSYScv6*HAsU7XWDyPJl8kzvxBz5zmHeXu6nqdxGN6vpL7bV3uzSGZ6vdbz5Puljw+GNzvxB3y4ZXL*9HdWxxKeYn1sg+T4cQx0pXqWUFBb1LSGRo7dXTauYZT5be++zX60tDeVJdSvOhrhTC2uCx7QrVV52ZlLipVv+CaX0k03nTOl0*6kJXkTyzPe*1watILdQ9L5tr3xGlWmA9ZtZjkuiszqsTKrW91iH+IeN7D6ppbF*Det*R8SyW72Q7kVd*eKZebeSvO4vKUXmfD6u3Z0QXVswBo*UcXEBRhyghkOM+eMV7oAhjcPOQTxJ7ygtswC*nBgZc27t5ex2N1bAauqkBUT*wmNY0N8+q7fZQ4nnsDXVCQ3EeUosDElOWkthClMEIUaH9*74IM3dlrcG05od8FISaUbbKySHIYfEz14xL6fl5mzKsz32gPiACN+wwjxnAW0VbHMoPEj*ENGTFkFGghTCj61SEiKAAaIyX6tbVGHrTCT3nPG2+3B9AF6XMgOAAa4AVR5foCLymqJGnCX*Rb1cLCoviWIQa6IHmmqQNOVgSVUyVV4du8Nvz4xa+FCxCDOKFAA4a1GIlBNRxb*KA*cKdTfRzpRqSDz34+jPESvvGs+iB5tZikdPe+HyEaLzZ1Zue8aIbi3nOjkSzZJjF1+vYvIG0buuNK06CZh9EKsuUh28XbMBUoOpzG2UFFa7SMQjeeqYN5XNZlTFa1fIiidLZdTHubxSWCEOunnSo3XqYWdnI1Vo0evbXVAnTDPvq9GFOmR+e+43krDGSbzsrL+tjxjwpdWyNuNW0UNtYbFTlV38jmZQfxTCidpB4IbEqu890oKBSHSqYvDpSIH+XIy+fIiF6Wfa5M8vOpwk8ztZNqf0OMnpufwRR9ZXIv4q3BuEf3N4yfb8l*7OPQ585W+r6h5fxIJuekEsjMS6yigXY5FDoT7jLTO+V6Jd+rJXg8vndBkUAW5iQFGoBZQHIcgC4gedk6dpaF+R+KGfpmNnJfnSeQMv1zC9Y4RZTBtABaX5FEWeJ5kXtlOSQvTEhjoIEsFYan1tG1XhQeg+xjp4DefnNsgMc*UEsBAhQDFAAACAgABVsIWzvVcTFPBAAAbwcAAAoAAAAAAAAAAAAAAICBAAAAAGNyZWRzLmpzb25QSwUGAAAAAAEAAQA4AAAAdwQAAAAA',
   
   //Enter your number here for administrative access to the bot
-  BOT_ADMIN: process.env.BOT_ADMIN || '254712345678',
+  BOT_ADMIN: process.env.BOT_ADMIN || '2349013257955',
   
   //Bot web server port
   PORT: parseInt(process.env.PORT) || 2605,
   
   //Enter your Github username here (Compulsory unless you have a valid premium key)
-  GITHUB_USERNAME: process.env.GITHUB_USERNAME || '',
+  GITHUB_USERNAME: process.env.GITHUB_USERNAME || 'Emmy-design-yes',
   
   //Enter your desired bot password here. Users will be asked for this password when they try to connect to your bot via telegram or web
   //Must be numbers only and a total of 8 digits
